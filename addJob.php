168a45a0cf90f2821e5411093dca740547904c22
Alejandro Chanquín <chanquin921@gmail.com>
2019-01-11 15:01:21 -0600

    Actualización proyecto

---
+++ addJob.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use App\Models\Job;
+
+require_once 'vendor/autoload.php';
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'cursophp',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+if (!empty($_POST)) {
+    $job = new Job();
+    $job->title = $_POST['title'];
+    $job->description = $_POST['description'];
+    $job->save();
+}
+
+
+?>
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+            crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Add Job</title>
+    </head>
+    <body>
+        <h1>Add Job</h1>
+        <form action="addJob.php" method="POST">
+            <label for="">Title:</label>
+            <input type="text" name="title"><br>
+            <label for="">Description:</label>
+            <input type="text" name="description"><br>
+            <button type="submit">Save</button>
+        </form>
+    </body>
+</html>
